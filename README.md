bf59081b7ead2a80c3b023960408629432812768
Matthew Conlen <mc@mathisonian.com>
2015-01-31 21:19:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+#  [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-url]][daviddm-image]
+
+> Node.js client for the Lightning data visualization server.
+
+
+## Install
+
+```sh
+$ npm install --save lightningjs
+```
+
+
+## Usage
+
+```js
+var Lightning = require('lightning.js');
+
+```
+
+
+## License
+
+MIT © [Matthew Conlen](http://mathisonian.com)
+
+
+[npm-url]: https://npmjs.org/package/lightningjs
+[npm-image]: https://badge.fury.io/js/lightningjs.svg
+[travis-url]: https://travis-ci.org/mathisonian/lightningjs
+[travis-image]: https://travis-ci.org/mathisonian/lightningjs.svg?branch=master
+[daviddm-url]: https://david-dm.org/mathisonian/lightningjs.svg?theme=shields.io
+[daviddm-image]: https://david-dm.org/mathisonian/lightningjs
---
+++ test/test.js
@@ -0,0 +1,73 @@
+/*global describe, it, done, before */
+'use strict';
+var expect = require('expect.js');
+var Lightning = require('../');
+var Visualization = require('../lib/visualization');
+var fs = require('fs');
+
+describe('lightningjs node module', function () {
+
+    var lightning;
+
+    before(function(done) {
+        lightning = new Lightning();
+        done();
+    });
+
+
+    it('should instantiate a Lightning object', function (done) {
+        expect(lightning).to.be.a(Lightning);
+        done();
+    });
+
+
+    it('should plot a line', function(done) {
+        lightning
+            .line([1,1,2,3,5,8,13,21])
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should work with an existing session', function(done) {
+        lightning.session = 1;
+        lightning
+            .line([1,1,2,3,5,8,13,21])
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should create a session', function(done) {
+        var sessionName = 'test-session';
+        lightning.createSession(sessionName)
+            .then(function(sessionId) {
+                expect(lightning.session).to.be(sessionId);
+                done();
+            });
+    });
+
+
+    it('should upload an image', function(done) {
+
+        lightning.image(fs.createReadStream(__dirname + '/img/example.png'))
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                done();
+            });
+    });
+
+    it('should upload a gallery', function(done) {
+
+        lightning.gallery([fs.createReadStream(__dirname + '/img/example.png'), fs.createReadStream(__dirname + '/img/example2.png')])
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                done();
+            });
+    });
+
+});
