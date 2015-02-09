bf91088a268bf6f5a48e7370a561b5313128d81d
Matthew Conlen <mc@mathisonian.com>
2015-02-09 01:17:57 -0500

    implement the visualization types

--- test/test.js
+++ test/test.js
@@ -33,6 +33,16 @@ describe('lightningjs node module', function () {
             });
     });
 
+    it('should plot a line-streaming', function(done) {
+        lightning
+            .lineStreaming([1,1,2,3,5,8,13,21])
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
     it('should plot a matrix', function(done) {
 
         var mat = _.map(_.range(100), function() {
@@ -50,7 +60,108 @@ describe('lightningjs node module', function () {
             });
     });
 
-    it.only('should plot a stacked line plot', function(done) {
+    it('should plot a scatter', function(done) {
+
+        var x = _.range(100);
+        var y = _.map(_.range(100), Math.random);
+
+        lightning
+            .scatter(x, y)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a scatter-streaming', function(done) {
+
+        var x = _.range(100);
+        var y = _.map(_.range(100), Math.random);
+
+        lightning
+            .scatterStreaming(x, y)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a scatter3', function(done) {
+
+        var x = _.range(100);
+        var y = _.map(_.range(100), function() {
+            return Math.random() * 100;
+        });
+        var z = _.map(_.range(100), function() {
+            return Math.random() * 100;
+        });
+
+        lightning
+            .scatter3(x, y, z)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a adjacency', function(done) {
+
+        var mat = _.map(_.range(3), function(i) {
+            return _.map(_.range(3), function(j) {
+                return i * 3 + j;
+            });
+        });
+
+        lightning
+            .adjacency(mat)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a force', function(done) {
+
+        var mat = _.map(_.range(13), function(i) {
+            return _.map(_.range(13), function(j) {
+                return i * 3 + j;
+            });
+        });
+
+        lightning
+            .force(mat)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a graph', function(done) {
+
+        var mat = _.map(_.range(10), function(i) {
+            return _.map(_.range(10), function(j) {
+                return i * 3 + j;
+            });
+        });
+
+        var x = _.range(10);
+        var y = _.map(_.range(10), Math.random);
+
+        lightning
+            .graph(x, y, mat)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should plot a stacked line plot', function(done) {
 
         var series = _.map(_.range(5), function() {
             return _.map(_.range(20), function() {
@@ -65,6 +176,33 @@ describe('lightningjs node module', function () {
                 expect(viz.id).to.not.be(null);
                 done();
             });
+    });
+
+    it('should make a map', function(done) {
+
+        var regions = ['NY', 'MI', 'NM'];
+        var values = [0.33, 0.6, 0.07];
+
+        lightning
+            .map(regions, values)
+            .then(function(viz) {
+                expect(viz).to.be.a(Visualization);
+                expect(viz.id).to.not.be(null);
+                done();
+            });
+    });
+
+    it('should not make a map', function(done) {
+
+        var regions = ['NY', 'MI', 'USA'];
+        var values = [0.33, 0.6, 0.07];
+        try {
+            lightning
+                .map(regions, values);
+        } catch(e) {
+            expect(e.toString()).to.eql('Error: All region names must be two letters (for US) or three letters (for world)');
+            done();
+        }
     });
 
     it('should work with an existing session', function(done) {
